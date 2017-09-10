62df2747aaf1e8437ac6e912493a4ebad2f7e9fd
EbenZhang <zhangyingneng@gmail.com>
2017-09-10 17:34:28 +0800

    init add source code

---
+++ Nicologies.ScopeGuard/ScopeGuard.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Nicologies.ScopeGuard
+{
+  public class ScopeGuard
+  {
+    private Action _exit;
+    public ScopeGuard(Action enter, Action exit)
+    {
+      enter();
+      _exit = exit;
+    }
+
+    public ScopeGuard(Action exit)
+    {
+      _exit = exit;
+    }
+    public void Dispose()
+    {
+      _exit();
+      _exit = null;
+    }
+  }
+}
